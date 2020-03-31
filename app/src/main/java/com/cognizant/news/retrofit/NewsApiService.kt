5ad580f51d9995ac83e642b901d2859479dee526
vishalkumar <vishalkumar.s@landmarkwebteam.com>
2020-03-31 20:04:17 +0530

    remove unwanted code and if rows model any null data is there we will not display cell in view list item

--- app/src/main/java/com/cognizant/news/retrofit/NewsApiService.kt
+++ app/src/main/java/com/cognizant/news/retrofit/NewsApiService.kt
@@ -1,10 +1,8 @@
 package com.cognizant.news.retrofit
 
 import com.cognizant.news.model.NewsArticles
-import io.reactivex.Observable
 import retrofit2.Call
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
@@ -19,7 +17,6 @@ interface NewsApiService {
     companion object Factory {
         fun create(): NewsApiService {
             val retrofit: Retrofit = Retrofit.Builder()
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl("https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/")
                 .build()
